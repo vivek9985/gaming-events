4aeacd7d050af2b4ad9ab47dcd15c5c347f91169
vivek9985 <vivekanando363@gmail.com>
2024-01-08 20:41:26 +0600

    modified contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,11 @@
-import contact from "../../assets/contact.jpg"
+import contact from "../../assets/contact.jpg";
 
 const Contact = () => {
   return (
     <div className="w-10/12 mx-auto my-52 md:my-32">
-      <h2 className="text-4xl text-center font-semibold mb-14">Contact with us</h2>
+      <h2 className="text-4xl text-center font-semibold mb-14">
+        Contact with us
+      </h2>
       <div className="grid gap-5 grid-cols-1 lg:grid-cols-2">
         <div className="p-5 lg:p-10 bg-fuchsia-50 rounded-2xl">
           <input
@@ -26,7 +28,9 @@ const Contact = () => {
             placeholder="Your message here ..."
             className="w-full py-3 px-5 mb-8 bg-white rounded-2xl focus:outline-none"
           ></textarea>
-          <button className="bg-purple-300 py-2 px-6 rounded-full mb-2 font-bold uppercase">Submit</button>
+          <button className="bg-purple-300 py-2 px-6 rounded-full mb-2 font-bold uppercase">
+            Submit
+          </button>
         </div>
         <div className="flex items-center justify-center rounded-2xl overflow-hidden">
           <img src={contact} alt="image" className="w-full h-full" />
